5099720c526607a512f91444d18357c535856e28
AndyMan <josueandy5@outlook.fr>
2024-03-03 08:59:17 +0100

    debut application

---
+++ src/main/java/com/andy/employe/model/Employee.java
@@ -0,0 +1,20 @@
+package com.andy.employe.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Data;
+
+@Entity
+@Table
+@Data
+@Builder
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
